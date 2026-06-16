163cab4daa8b34b2f293a76f688af011666f1697
manuvdb <vanderbruggen.emmanuel@gmail.com>
2026-06-16 11:22:54 +0200

    add models InventoryBike and SpareParts

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,4 @@
 from models.client import Client
 from models.client_bike import ClientBike
+from models.sale import SaleLineBike
+from models.sale import SaleLinePart
---
+++ models/sale.py
@@ -0,0 +1,7 @@
+from db.database import Base
+
+class SaleLineBike(Base):
+    pass
+
+class SaleLinePart(Base):
+    pass
