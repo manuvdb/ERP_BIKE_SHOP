0ad787338785d024d023fde2a88e2d211c5ebf27
manuvdb <vanderbruggen.emmanuel@gmail.com>
2026-06-16 10:48:13 +0200

    Feat : add models Client and ClientBike with relationship

---
+++ models/__init__.py
@@ -0,0 +1,2 @@
+from models.client import Client
+from models.client_bike import ClientBike
---
+++ models/client_bike.py
@@ -0,0 +1,23 @@
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey, Identity, String
+from db.database import Base
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from models import Client
+
+class ClientBike(Base):
+    __tablename__ = "client_bikes"
+
+    id: Mapped[int] = mapped_column(Identity(always=True), primary_key=True)
+    client_id: Mapped[int] = mapped_column(ForeignKey("clients.id"), nullable=False)
+    brand: Mapped[str | None] = mapped_column(String(50), nullable=True)
+    model: Mapped[str | None] = mapped_column(String(50), nullable=True)
+    type: Mapped[str] = mapped_column(String(50), nullable=False)
+    color: Mapped[str] = mapped_column(String(20), nullable=False)
+    serial_number: Mapped[str | None] = mapped_column(String(100), unique=True, nullable=True)
+
+    client: Mapped["Client"] = relationship("Client", back_populates="bikes")
+    
+    def __repr__(self):
+        return f"ClientBike(id={self.id}, client_id={self.client_id}, type={self.type}, color={self.color})"
